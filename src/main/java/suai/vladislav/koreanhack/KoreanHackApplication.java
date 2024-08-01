b2a8d904a120b9d7cfbe08b49968f863a45ae97d
ANANAzZzZz <vladscooltrol@gmail.com>
2024-08-01 17:42:46 +0300

    initial commit

---
+++ src/main/java/suai/vladislav/koreanhack/KoreanHackApplication.java
@@ -0,0 +1,13 @@
+package suai.vladislav.koreanhack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreanHackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreanHackApplication.class, args);
+    }
+
+}
